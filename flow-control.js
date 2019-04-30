685126526089eaa19c351b0b03e66b31b83f3d91
Kailee Gray <kaileeagray@gmail.com>
2019-04-30 16:59:13 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -13,7 +13,7 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  if (age >= 13 && age <= 19) {
+  else if (age >= 13 && age <= 19) {
     return "You are a teenager!";
   } else {
     return "You are not a teenager";
